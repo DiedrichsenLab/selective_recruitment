84db40d9d738211fd2036e3975d1d4bbe66460db
lshahsha <lshahsha@uwo.ca>
2023-01-10 15:58:45 -0500

    added function to make df for roi-wise scatterplot - work in progress

--- select_recruite.py
+++ select_recruite.py
@@ -80,27 +80,34 @@ def extract_fs32K(dataset_name, ses_id, type):
     return
 
 # 3. aggregate data for each parcel within the parcellation
-def agg_data_parcel(data, info, atlas, label_img):
+def agg_data_parcel(data, info, atlas, label_img, unite_hemi = True):
     """
     aggregate data over the voxels/vertices within a parcel.
     Args:
     data (np.ndarray) - data to be aggregated
     atlas (Atlas obj) - an instance of the atlas object
     label_img (list)  - str or list of str representing the label images
+    mask_img (list)   - list of mask str representing paths to the mask used when creating the data
 
     Returns:
     data_parcel (np.ndarray) - numpy array #condition-by-#parcels      
     """
 
     # get parcel and parcel axis using the label image
-    atlas.get_parcel(label_img=label_img)
+    atlas.get_parcel(label_img=label_img, unite_hemi = unite_hemi)
     atlas.get_parcel_axis()
 
     if len(atlas.label_vector) == 1: # for cerebellum (not divided by hemi)
         vector = atlas.label_vector[0]
     else: # for cortex, divided into left and right
         vector = np.concatenate(atlas.label_vector, axis = 0)
-
+        # get the mask used to create the atlas
+        mask = np.concatenate(atlas.mask, axis = 0)
+        # apply the mask
+        # NOTE: this could be deleted if we apply the left and right hemi mask to 
+        # all the surface label giftis we have under atl-fs32k
+        vector = vector[mask > 0]    
+    
     # create a matrix for aggregating data (cannot use dataset.agg_data now! Need to make changes)
     C = indicator(vector,positive=True)
 
@@ -204,7 +211,7 @@ def get_summary_whole(dataset_name):
     return summary_df
 
 # 6. getting data into a dataframe
-def get_summary_roi(dataset_name = "WMFS"):
+def get_summary_roi(dataset_name = "WMFS", info_subset = None):
     """
     prepares a dataframe for plotting the scatterplot
     """
@@ -224,11 +231,11 @@ def get_summary_roi(dataset_name = "WMFS"):
     atlas_cortex = AtlasSurface(name, mask_img=mask_image, structure=["cortex_left", 'cortex_right'])
 
     # get label images for the cerebellum and cortex
-    label_cereb = atlas_dir + '/tpl-SUIT' + '/atl-MDTB10_space-SUIT_dseg.nii'
+    label_cereb = atlas_dir + '/tpl-SUIT' + '/atl-Buckner7_space-SUIT_dseg.nii'
 
     label_cortex = []
     for hemi in ['L', 'R']:
-        label_cortex.append(atlas_dir + '/tpl-fs32k' + f'/Icosahedron-42_Sym.32k.{hemi}.label.gii')
+        label_cortex.append(atlas_dir + '/tpl-fs32k' + f'/yeo7.{hemi}.label.gii')
 
 
     # getting data for all the participants in SUIT space
@@ -236,18 +243,48 @@ def get_summary_roi(dataset_name = "WMFS"):
     # getting data for all the participants in fs32k space
     ccdat, info = Dat.get_data(space='fs32k', ses_id='ses-02', type='CondHalf', fields=None)
 
-    # loop through subjects
-    for sub in range(T.participant_id):
+    # loop through subjects and create a dataframe
+    summary_list = []
+    for sub in range(len(T.participant_id)):
+        
         # get data for the current subject
         this_data_cereb = cdat[sub, :, :]
 
         this_data_cortex = ccdat[sub, :, :]
 
         # pass on the data with the atlas object to the aggregating function
-        cifti_Y = agg_data_parcel(this_data_cereb, info, atlas_cereb, label_cereb)
-        cifti_X = agg_data_parcel(this_data_cortex, info, atlas_cortex, label_cortex)
-        print("- GOT CIFTIS")
-    return
+        cifti_Y = agg_data_parcel(this_data_cereb, info, atlas_cereb, label_cereb, unite_hemi=False)
+        cifti_X = agg_data_parcel(this_data_cortex, info, atlas_cortex, label_cortex, unite_hemi=True)
+
+        # getting parcel axis 
+        # pax = cifti_X.header.get_axis(1)
+        pay = cifti_Y.header.get_axis(1)
+
+        # get data per parcel
+        X = cifti_X.get_fdata()
+        Y = cifti_Y.get_fdata()
+
+        # looping over labels and doing regression for each corresponding label
+        for ilabel in range(len(pay.name)):
+            info_sub = info.copy()
+            print(f"- subject {T.participant_id[sub]} label {ilabel+1}")
+            x = X[:, ilabel]
+            y = Y[:, ilabel]
+
+            coef, res, R2 = regressXY(x, y, subtract_mean = False)
+
+            info_sub["sn"] = T.participant_id[sub]
+            info_sub["X"] = x
+            info_sub["Y"] = y
+            info_sub["res"] = res
+            info_sub["coef"] = coef * np.ones([len(info), 1])
+            info_sub["R2"] = R2 * np.ones([len(info), 1])
+            info_sub['label'] = (ilabel+1) * np.ones([len(info), 1])
+
+            summary_list.append(info_sub)
+        
+    summary_df = pd.concat(summary_list, axis = 0) 
+    return summary_df
 
 
 
@@ -260,12 +297,14 @@ if __name__ == "__main__":
     # df = get_summary_whole('WMFS')
     # # save the dataframe for later
     # filepath = os.path.join(base_dir, 'WMFS', 'sc_df_whole_ses-02.tsv')
-    # df.to_csv(filepath, index=False,sep='\t')
+    # df.to_csv(filepath, index = False, sep='\t')
     
 
     """
     Getting the summary dataframe for the scatterplot in an ROI-wise manner
     """
-    get_summary_roi(dataset_name = "WMFS")
-
+    df = get_summary_roi(dataset_name = "WMFS")
+    # save the dataframe for later
+    filepath = os.path.join(base_dir, 'WMFS', 'sc_df_buckner7_ses-02.tsv')
+    df.to_csv(filepath, index = False, sep='\t')
 

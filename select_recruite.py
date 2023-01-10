4f94c0e97e5bc6b7885102dd550521e543894ebc
lshahsha <lshahsha@uwo.ca>
2023-01-10 16:33:41 -0500

    cleaning up the code, removing unnecessary function

--- select_recruite.py
+++ select_recruite.py
@@ -92,13 +92,13 @@ def agg_data_parcel(data, info, atlas, label_img, unite_hemi = True):
     Returns:
     data_parcel (np.ndarray) - numpy array #condition-by-#parcels      
     """
-
     # get parcel and parcel axis using the label image
     atlas.get_parcel(label_img=label_img, unite_hemi = unite_hemi)
     atlas.get_parcel_axis()
 
     if len(atlas.label_vector) == 1: # for cerebellum (not divided by hemi)
         vector = atlas.label_vector[0]
+
     else: # for cortex, divided into left and right
         vector = np.concatenate(atlas.label_vector, axis = 0)
         # get the mask used to create the atlas
@@ -127,7 +127,17 @@ def agg_data_parcel(data, info, atlas, label_img, unite_hemi = True):
     return cifti_img
 
 # 4. OPTIONAL step: use connectivity model to predict cerebellar activation
-def predict_cerebellum():
+def predict_cerebellum(X, w):
+    """
+    makes predictions for the cerebellar activation
+    uses weights from a linear model (w) and cortical data (X)
+    to make predictions Yhat
+    Args:
+    X (np.ndarray)
+    w (np.ndarray)
+    Returns:
+    Yhat (np.ndarray)
+    """
     return
 
 # 5. regress cerebellar data onto step 4 (or 4:alternative)
@@ -170,48 +180,10 @@ def regressXY(X, Y, subtract_mean = False):
     return coef, residual, R2
 
 # 6. getting data into a dataframe
-def get_summary_whole(dataset_name):
-    """
-    prepares a dataframe for plotting the scatterplot with X and Y averaged over the whole cerebellum and cortex
-    """
-    # get the dataset class
-    Dat = get_class(dataset_name= dataset_name)
-    # get participants for the dataset
-    T = Dat.get_participants()
-
-    # getting data for all the participants in SUIT space
-    cdat, info = Dat.get_data(space='SUIT3', ses_id='ses-02', type='CondHalf', fields=None)
-    # getting data for all the participants in fs32k space
-    ccdat, _ = Dat.get_data(space='fs32k', ses_id='ses-02', type='CondHalf', fields=None)
-
-    # average over the whole structures
-    Y = np.nanmean(cdat, axis = 2)
-    X = np.nanmean(ccdat, axis = 2)
-
-    # looping through subjects and doing regression for each subject
-    summary_list = []
-    for sub in range(len(T.participant_id)):
-        info_sub = info.copy()
-        print(f"- getting info for {T.participant_id[sub]}")
-        x = X[sub, :]
-        y = Y[sub, :]
-
-        coef, res, R2 = regressXY(x, y, subtract_mean = False)
-        
-        info_sub["sn"] = T.participant_id[sub]
-        info_sub["X"] = x
-        info_sub["Y"] = y
-        info_sub["res"] = res
-        info_sub["coef"] = coef * np.ones([len(info), 1])
-        info_sub["R2"] = R2 * np.ones([len(info), 1])
-
-        summary_list.append(info_sub)
-    
-    summary_df = pd.concat(summary_list, axis = 0)
-    return summary_df
-
-# 6. getting data into a dataframe
-def get_summary_roi(dataset_name = "WMFS", info_subset = None):
+def get_summary(dataset_name = "WMFS",
+                cerebellum = 'Buckner7', 
+                cortex = 'yeo7', 
+                agg_whole = False):
     """
     prepares a dataframe for plotting the scatterplot
     """
@@ -221,21 +193,27 @@ def get_summary_roi(dataset_name = "WMFS", info_subset = None):
     T = Dat.get_participants()
 
     # create instances of atlases for the cerebellum and cortex
-    mask_image = atlas_dir + '/tpl-SUIT' + '/tpl-SUIT_res-3_gmcmask.nii'
-    atlas_cereb = AtlasVolumetric('SUIT3', mask_img=mask_image, structure="cerebellum")
+    mask_cereb = atlas_dir + '/tpl-SUIT' + '/tpl-SUIT_res-3_gmcmask.nii'
+    atlas_cereb = AtlasVolumetric('SUIT3', mask_img=mask_cereb, structure="cerebellum")
 
-    mask_image = []
+    mask_cortex = []
     for hemi in ['L', 'R']:
-        mask_image.append(atlas_dir + '/tpl-fs32k' + f'/tpl-fs32k_hemi-{hemi}_mask.label.gii')
-        name = 'fs32k'
-    atlas_cortex = AtlasSurface(name, mask_img=mask_image, structure=["cortex_left", 'cortex_right'])
+        mask_cortex.append(atlas_dir + '/tpl-fs32k' + f'/tpl-fs32k_hemi-{hemi}_mask.label.gii')
+    name = 'fs32k'
+    atlas_cortex = AtlasSurface(name, mask_img=mask_cortex, structure=["cortex_left", 'cortex_right'])
 
     # get label images for the cerebellum and cortex
-    label_cereb = atlas_dir + '/tpl-SUIT' + '/atl-Buckner7_space-SUIT_dseg.nii'
+    if agg_whole: # using masks as labels
+        # using 1 label over the whole structure (mask = 0 where there's no data mask = 1 otherwise)
+        label_cereb = mask_cereb
+        label_cortex = mask_cortex
+    else:
 
-    label_cortex = []
-    for hemi in ['L', 'R']:
-        label_cortex.append(atlas_dir + '/tpl-fs32k' + f'/yeo7.{hemi}.label.gii')
+        label_cereb = atlas_dir + '/tpl-SUIT' + f'/atl-{cerebellum}_space-SUIT_dseg.nii'
+
+        label_cortex = []
+        for hemi in ['L', 'R']:
+            label_cortex.append(atlas_dir + '/tpl-fs32k' + f'/{cortex}.{hemi}.label.gii')
 
 
     # getting data for all the participants in SUIT space
@@ -256,16 +234,12 @@ def get_summary_roi(dataset_name = "WMFS", info_subset = None):
         cifti_Y = agg_data_parcel(this_data_cereb, info, atlas_cereb, label_cereb, unite_hemi=False)
         cifti_X = agg_data_parcel(this_data_cortex, info, atlas_cortex, label_cortex, unite_hemi=True)
 
-        # getting parcel axis 
-        # pax = cifti_X.header.get_axis(1)
-        pay = cifti_Y.header.get_axis(1)
-
         # get data per parcel
         X = cifti_X.get_fdata()
         Y = cifti_Y.get_fdata()
 
         # looping over labels and doing regression for each corresponding label
-        for ilabel in range(len(pay.name)):
+        for ilabel in range(Y.shape[1]):
             info_sub = info.copy()
             print(f"- subject {T.participant_id[sub]} label {ilabel+1}")
             x = X[:, ilabel]
@@ -290,21 +264,20 @@ def get_summary_roi(dataset_name = "WMFS", info_subset = None):
 
 
 if __name__ == "__main__":
-    """
-    Getting the summary dataframe for the scatterplot over whole structures
-    """
-    # print("Using WMFS dataset")
-    # df = get_summary_whole('WMFS')
-    # # save the dataframe for later
-    # filepath = os.path.join(base_dir, 'WMFS', 'sc_df_whole_ses-02.tsv')
-    # df.to_csv(filepath, index = False, sep='\t')
-    
 
     """
     Getting the summary dataframe for the scatterplot in an ROI-wise manner
     """
-    df = get_summary_roi(dataset_name = "WMFS")
+    df = get_summary(dataset_name = "WMFS", agg_whole=False)
     # save the dataframe for later
-    filepath = os.path.join(base_dir, 'WMFS', 'sc_df_buckner7_ses-02.tsv')
+    filepath = os.path.join(base_dir, 'WMFS', 'sc_df_yeo_buckner_7_ses-02.tsv')
+    df.to_csv(filepath, index = False, sep='\t')
+
+    """
+    Getting the summary dataframe for the scatterplot over whole structures
+    """
+    df = get_summary(dataset_name = "WMFS", agg_whole=True)
+    # save the dataframe for later
+    filepath = os.path.join(base_dir, 'WMFS', 'sc_df_whole_ses-02.tsv')
     df.to_csv(filepath, index = False, sep='\t')
 

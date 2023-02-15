608db0886dc700a10faaca6986b89d41cde63fe5
lshahsha <lshahsha@uwo.ca>
2023-02-15 16:32:39 -0500

    code cleanup

--- scripts/script_prep_sc.py
+++ scripts/script_prep_sc.py
@@ -22,60 +22,39 @@ if not Path(base_dir).exists():
 atlas_dir = base_dir + '/Atlases'
 
 
-# run this function to make sure that you have saved data tensors
-def prep_tensor(dataset_name = "WMFS", ses_id = 'ses-02'):
-   cdata.save_data_tensor(dataset = dataset_name,
-                         atlas='SUIT3',
-                         ses_id=ses_id,
-                         type="CondHalf")
-   cdata.save_data_tensor(dataset = dataset_name,
-                         atlas='fs32k',
-                         ses_id=ses_id,
-                         type="CondHalf")
-   return
+def get_data(tensor, atlas, label, unite_struct = True):
+    """
+    gets the data ready for regression by aggregating over the parcels
 
-# prepare data for cortex and cerebellum
-def get_XY(label_cereb, 
-           label_cortex,
-           file_path = cdata.conn_dir, 
-           dataset = "WMFS", 
-           ses_id = "ses-02",
-           type = "CondHalf", 
-           atlas_cereb = "SUIT3",
-           atlas_cortex = "fs32k",
-           unite_struct = False
-           ):
-    # get dataset class object
-    Data = fdata.get_dataset_class(base_dir, dataset=dataset)
+    """
+    # get data tensor
+    atlas, ainfo = am.get_atlas(atlas,atlas_dir)
 
-    # get info
-    info = Data.get_info(ses_id,type)
+    # NOTE: atlas.get_parcel takes in path to the label file, not an array
+    if (isinstance(atlas, am.AtlasSurface)) | (isinstance(atlas, am.AtlasSurfaceSymmetric)):
+        if label is not None:
+            atlas.get_parcel(label, unite_struct = unite_struct)
+        else: # passes on mask to get parcel if you want the average across the whole structure
+            atlas.get_parcel(ainfo['mask'], unite_struct) 
 
-    # load data tensor for SUIT3 (TODO: add an option to specify atlases other than SUIT3 and fs32k)
-    file_suit = file_path + f'/{dataset}/{dataset}_SUIT3_{ses_id}_{type}.npy'
-    Ydat = np.load(file_suit)
+    else:
+        if label is not None:
+            atlas.get_parcel(label)
+        else: # passes on mask to get parcel if you want the average across the whole structure
+            atlas.get_parcel(ainfo['mask']) 
 
-    # load data tensor for fs32k
-    file_fs32k = file_path + f'/{dataset}/{dataset}_fs32k_{ses_id}_{type}.npy'
-    Xdat = np.load(file_fs32k)
-    
-    # create instances of atlases for the cerebellum and cortex
-    atlas_cereb, ainfo = am.get_atlas('SUIT3',atlas_dir)
-    atlas_cortex, ainfo = am.get_atlas('fs32k', atlas_dir)
 
-    # get parcel for both atlases
-    atlas_cereb.get_parcel(label_cereb)
-    atlas_cortex.get_parcel(label_cortex, unite_struct = unite_struct)
+    # aggregate over voxels/vertices within parcels
+    data, parcels_cereb = fdata.agg_parcels(tensor , atlas.label_vector, fcn=np.nanmean)
+    return data, info, parcels_cereb
 
-    # aggregate data over parcels
-    X_parcel = fdata.agg_parcels(Xdat , atlas_cortex.label_vector,fcn=np.nanmean)
-    Y_parcel = fdata.agg_parcels(Ydat , atlas_cereb.label_vector,fcn=np.nanmean)
-    return X_parcel, Y_parcel, info
-
-def get_summary_whole(outpath = cdata.conn_dir, 
-                      dataset = "WMFS", 
-                      ses_id = 'ses-02', 
-                      type = "CondHalf"):
+def get_summary(outpath = cdata.conn_dir, 
+                dataset = "WMFS", 
+                ses_id = 'ses-02', 
+                type = "CondAll", 
+                cerebellum =None, 
+                cortex = None, 
+                unite_struct = False):
 
     """
     get summary dataframe to plot scatterplot for average ceerbellum vs average cortex
@@ -87,67 +66,30 @@ def get_summary_whole(outpath = cdata.conn_dir,
     Returns:
         summ_df (pd.DataFrame) - summary dataframe to be saved and used for plotting
     """
-
+    tensor_cerebellum, info, _ = fdata.get_dataset(base_dir,dataset,atlas="SUIT3",sess=ses_id,type=type, info_only=False)
+    tensor_cortex, info, _ = fdata.get_dataset(base_dir,dataset,atlas="fs32k",sess=ses_id,type=type, info_only=False)
+    
     # get label files for cerebellum and cortex
-    # NOTE: To average over cerebellum or cortex, pass on masks as label files
-    label_cereb = atlas_dir + '/tpl-SUIT/' + 'tpl-SUIT_res-3_gmcmask.nii'
-    label_cortex = []
-    for hemi in ['L', 'R']:
-        label_cortex.append(atlas_dir + '/tpl-fs32k' + f'/tpl-fs32k_hemi-{hemi}_mask.label.gii')
+    ## if None is passed then it will be averaged over the whole
+    if cerebellum is not None:
+        cerebellum_label = atlas_dir + '/tpl-SUIT' + f'/atl-{cerebellum}_space-SUIT_dseg.nii'
+    if cortex is not None:
+        cortex_label = []
+        for hemi in ['L', 'R']:
+            cortex_label.append(atlas_dir + '/tpl-fs32k' + f'/{cortex}.{hemi}.label.gii')
 
-    X_parcel, Y_parcel, info = get_XY(label_cereb, 
-                                      label_cortex,
-                                      file_path = cdata.conn_dir, 
-                                      dataset = dataset, 
-                                      ses_id = ses_id,
-                                      atlas_cereb = "SUIT3",
-                                      atlas_cortex = "fs32k",
-                                      type = type, 
-                                      unite_struct=True
-                                     )
+    # get the data for all the subjects for cerebellum
+    Y_parcel, info, cerebellum_parcel = get_data(tensor_cerebellum, atlas = "SUIT3", label = cerebellum_label)
+    X_parcel, info, cortex_parcel = get_data(tensor_cortex, atlas = "fs32k", label = cortex_label)
+    
 
-    summ_df = ra.run_regress(X_parcel,Y_parcel,info,fit_intercept = False)
+    # do regression and get the summary dataframe 
+    D = ra.run_regress(X_parcel,Y_parcel,info,fit_intercept = False)
 
     # save the dataframe
-    filepath = os.path.join(outpath, dataset, f'sc_{dataset}_{ses_id}_whole.tsv')
-    summ_df.to_csv(filepath, index = False, sep='\t')
+    filepath = os.path.join(outpath, dataset, f'sc_{dataset}_{ses_id}_{cerebellum}_{cortex}.tsv')
+    D.to_csv(filepath, index = False, sep='\t')
 
-    return summ_df
-
-def get_summary_roi(outpath = cdata.conn_dir, 
-                    dataset = "WMFS", 
-                    ses_id = 'ses-02', 
-                    cerebellum = "Verbal2Back", 
-                    cortex = "Verbal2Back.32k", 
-                    type = "CondHalf"):
-
-    """
-    get summary dataframe for roi-wise analysis
-    """
-
-    # get label files for cerebellum and cortex
-    # NOTE: To average over cerebellum or cortex, pass on masks as label files
-    label_cereb = atlas_dir + '/tpl-SUIT' + f'/atl-{cerebellum}_space-SUIT_dseg.nii'
-    label_cortex = []
-    for hemi in ['L', 'R']:
-        label_cortex.append(atlas_dir + '/tpl-fs32k' + f'/{cortex}.{hemi}.label.gii')
-
-    X_parcel, Y_parcel, info = get_XY(label_cereb, 
-                                      label_cortex,
-                                      file_path = cdata.conn_dir, 
-                                      dataset = dataset, 
-                                      ses_id = ses_id,
-                                      atlas_cereb = "SUIT3",
-                                      atlas_cortex = "fs32k",
-                                      type = type, 
-                                      unite_struct=True
-                                     )
-
-    summ_df = ra.run_regress(X_parcel,Y_parcel,info,fit_intercept = False)
-
-    # save the dataframe
-    filepath = os.path.join(outpath, dataset, f'sc_{dataset}_{ses_id}_{cerebellum}.tsv')
-    summ_df.to_csv(filepath, index = False, sep='\t')
     return
 
 def get_summary_conn(outpath = cdata.conn_dir, 
@@ -164,7 +106,9 @@ def get_summary_conn(outpath = cdata.conn_dir,
     """
     """
     
-
+    tensor_cerebellum, info, _ = fdata.get_dataset(base_dir,dataset,atlas="SUIT3",sess=ses_id,type=type, info_only=False)
+    tensor_cortex, info, _ = fdata.get_dataset(base_dir,dataset,atlas="fs32k",sess=ses_id,type=type, info_only=False)
+    
     # get label files for cerebellum and cortex
     # NOTE: To average over cerebellum or cortex, pass on masks as label files
     label_cereb = atlas_dir + '/tpl-SUIT' + f'/atl-{cerebellum}_space-SUIT_dseg.nii'
@@ -179,45 +123,48 @@ def get_summary_conn(outpath = cdata.conn_dir,
 
     atlas_cereb, ainfo = am.get_atlas('SUIT3',atlas_dir)
     atlas_cereb.get_parcel(label_cereb)
-    X_parcel, Y_parcel, info = get_XY(label_cereb, 
-                                      label_cortex,
-                                      file_path = cdata.conn_dir, 
-                                      dataset = dataset, 
-                                      ses_id = ses_id,
-                                      atlas_cereb = "SUIT3",
-                                      atlas_cortex = "fs32k",
-                                      type = type,
-                                      unite_struct=False
-                                     )
+    # get label files for cerebellum and cortex
+    ## if None is passed then it will be averaged over the whole
+    if cerebellum is not None:
+        cerebellum_label = atlas_dir + '/tpl-SUIT' + f'/atl-{cerebellum}_space-SUIT_dseg.nii'
+    if cortex is not None:
+        cortex_label = []
+        for hemi in ['L', 'R']:
+            cortex_label.append(atlas_dir + '/tpl-fs32k' + f'/{cortex}.{hemi}.label.gii')
+
+    # get the data for all the subjects for cerebellum
+    Y_parcel, info, _ = get_data(dataset = dataset, ses_id=ses_id, type = type, atlas = "SUIT3", label = cerebellum_label)
+
+    X_parcel, info, _ = get_data(dataset = dataset, ses_id=ses_id, type = type, atlas = "fs32k", label = cortex_label)
+    
     # use connectivity weights to predict
     Yhat = ra.predict_cerebellum(weights, scale, X_parcel, atlas_cereb, info, fwhm = 0)
 
     # aggregate values over voxel within parcel
-    Yhat_parcel = fdata.agg_parcels(Yhat , atlas_cereb.label_vector,fcn=np.nanmean)
+    Yhat_parcel, parcels = get_data(Yhat, atlas = "SUIT3", label = cerebellum_label)
 
     # run regression (use Yhat as X)
     summ_df_list = []
     # loop over parcels
     for p in range(Yhat_parcel.shape[2]):
-        summ_df_list.append(ra.run_regress(Yhat_parcel[:, :, p],Y_parcel[:, :, p],info,fit_intercept = False))
+        D = ra.run_regress(Yhat_parcel[:, :, parcels[p]],Y_parcel[:, :, parcels[p]],info,fit_intercept = False)
+        D['region'] = parcels[p] *np.ones([len(D.index), ])
+        summ_df_list.append(D)
 
     # concatenate dataframe
     summ_df = pd.concat(summ_df_list)
 
     # save the dataframe
-    filepath = os.path.join(outpath, dataset, f'sc_{dataset}_{ses_id}_conn.tsv')
+    filepath = os.path.join(outpath, dataset, f'sc_{dataset}_{ses_id}_{cerebellum}_conn.tsv')
     summ_df.to_csv(filepath, index = False, sep='\t')
-
-
     return summ_df
 
 
 if __name__ == "__main__":
-    # prep_tensor(dataset_name = "WMFS", ses_id = "ses-01")
-    # prep_tensor(dataset_name = "WMFS", ses_id = "ses-01")
-
-    get_summary_whole(outpath = cdata.conn_dir, 
-                      dataset = "WMFS", 
-                      ses_id = 'ses-02', 
-                      type = "CondHalf")
-    
+    get_summary(outpath = cdata.conn_dir, 
+                dataset = "WMFS", 
+                ses_id = 'ses-02', 
+                type = "CondAll", 
+                cerebellum ="Verbal2Back", 
+                cortex = "Verbal2Back.32k", 
+                unite_struct = True)

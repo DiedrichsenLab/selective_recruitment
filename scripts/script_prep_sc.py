6a1b145a6b5f561d640ab099f87785bb615c75d9
lshahsha <lshahsha@uwo.ca>
2023-02-21 19:02:05 -0500

    used refactorized code to make summary dfs

--- scripts/script_prep_sc.py
+++ scripts/script_prep_sc.py
@@ -1,6 +1,6 @@
 """
 script to prepare dataframe for scatterplots
-@ Ladan Shahshahani Jan 30 2023 12:57
+@ Ladan Shahshahani Joern Diedrichsen Feb 19 2023
 """
 import os
 import numpy as np
@@ -11,11 +11,12 @@ from collections import defaultdict
 import nibabel as nb
 import Functional_Fusion.dataset as fdata 
 import Functional_Fusion.atlas_map as am
-# import cortico_cereb_connectivity.data as cdata 
 import selective_recruitment.recruite_ana as ra
 import selective_recruitment.globals as gl
 
-outdir = '/Volumes/diedrichsen_data$/data/Cerebellum/CerebellumWorkingMemory/selective_recruit'
+out_dir = '/Volumes/diedrichsen_data$/data/Cerebellum/CerebellumWorkingMemory/selective_recruit'
+if not Path(out_dir).exists():
+    out_dir = '/srv/diedrichsen/data/Cerebellum/CerebellumWorkingMemory/selective_recruit'
 
 
 def get_summary_conn(dataset = "WMFS", 
@@ -23,70 +24,136 @@ def get_summary_conn(dataset = "WMFS",
                      cerebellum = "Verbal2Back", 
                      cortex = "Icosahedron-1002_Sym.32k",
                      type = "CondHalf", 
+                     add_rest = True,
                      conn_dataset = "MDTB", 
                      conn_method = "L2Regression", 
                      log_alpha = 8, 
                      conn_ses_id = "ses-s1"):
 
     """
+    Function to get summary dataframe using connectivity model to predict cerebellar activation.
+    It's written similar to get_symmary from recruite_ana code
     """
     
-    tensor_cerebellum, info, _ = fdata.get_dataset(base_dir,dataset,atlas="SUIT3",sess=ses_id,type=type, info_only=False)
-    tensor_cortex, info, _ = fdata.get_dataset(base_dir,dataset,atlas="fs32k",sess=ses_id,type=type, info_only=False)
-    
-    # get label files for cerebellum and cortex
-    # NOTE: To average over cerebellum or cortex, pass on masks as label files
-    label_cereb = atlas_dir + '/tpl-SUIT' + f'/atl-{cerebellum}_space-SUIT_dseg.nii'
-    label_cortex = []
-    for hemi in ['L', 'R']:
-        label_cortex.append(atlas_dir + '/tpl-fs32k' + f'/{cortex}.{hemi}.label.gii')
+    tensor_cerebellum, info, _ = fdata.get_dataset(gl.base_dir,dataset,atlas="SUIT3",sess=ses_id,type=type, info_only=False)
+    tensor_cortex, info, _ = fdata.get_dataset(gl.base_dir,dataset,atlas="fs32k",sess=ses_id,type=type, info_only=False)
 
     # get connectivity weights and scaling 
-    conn_dir = os.path.join(cdata.conn_dir, conn_dataset, "train")
+    conn_dir = os.path.join(gl.conn_dir, conn_dataset, "train")
     weights = np.load(os.path.join(conn_dir, f"{cortex}_{conn_ses_id}_{conn_method}_logalpha_{log_alpha}_best_weights.npy"))
     scale = np.load(os.path.join(conn_dir, f'{conn_dataset}_scale.npy'))
 
-    atlas_cereb, ainfo = am.get_atlas('SUIT3',atlas_dir)
-    atlas_cereb.get_parcel(label_cereb)
-    # get label files for cerebellum and cortex
-    ## if None is passed then it will be averaged over the whole
-    if cerebellum is not None:
-        cerebellum_label = atlas_dir + '/tpl-SUIT' + f'/atl-{cerebellum}_space-SUIT_dseg.nii'
-    if cortex is not None:
-        cortex_label = []
-        for hemi in ['L', 'R']:
-            cortex_label.append(atlas_dir + '/tpl-fs32k' + f'/{cortex}.{hemi}.label.gii')
+    # prepare the cortical data 
+    # NOTE: to use connectivity weights estimated in MDTB you always need to pass a tesselation
+    # BECAUSE the models have been trained using tesselations
+    cortex_label = []
+    for hemi in ['L', 'R']:
+        cortex_label.append(gl.atlas_dir + '/tpl-fs32k' + f'/{cortex}.{hemi}.label.gii')
+    X_parcel, ainfo, X_parcel_labels = ra.agg_data(tensor_cortex, "fs32k", cortex_label, unite_struct = False)
 
-    # get the data for all the subjects for cerebellum
-    Y_parcel, info, _ = get_data(dataset = dataset, ses_id=ses_id, type = type, atlas = "SUIT3", label = cerebellum_label)
-
-    X_parcel, info, _ = get_data(dataset = dataset, ses_id=ses_id, type = type, atlas = "fs32k", label = cortex_label)
-    
-    # use connectivity weights to predict
+    # use cortical data to predict cerebellar data (voxel-wise)
+    atlas_cereb, _ = am.get_atlas("SUIT3",gl.atlas_dir)
     Yhat = ra.predict_cerebellum(weights, scale, X_parcel, atlas_cereb, info, fwhm = 0)
 
-    # aggregate values over voxel within parcel
-    Yhat_parcel, parcels = get_data(Yhat, atlas = "SUIT3", label = cerebellum_label)
+    # get the cerebellar data
+    # NOTE: if None is passed, then it will average over the whole cerebellum
+    if cerebellum is not None:
+        cerebellum_label = gl.atlas_dir + '/tpl-SUIT' + f'/atl-{cerebellum}_space-SUIT_dseg.nii'
+        
 
-    # run regression (use Yhat as X)
-    summ_df_list = []
-    # loop over parcels
-    for p in range(Yhat_parcel.shape[2]):
-        D = ra.run_regress(Yhat_parcel[:, :, parcels[p]],Y_parcel[:, :, parcels[p]],info,fit_intercept = False)
-        D['region'] = parcels[p] *np.ones([len(D.index), ])
-        summ_df_list.append(D)
+        # get observed cerebellar data
+        Y_parcel, ainfo, Y_parcel_labels = ra.agg_data(tensor_cerebellum, "SUIT3", cerebellum_label, unite_struct = False)
 
-    # concatenate dataframe
-    summ_df = pd.concat(summ_df_list)
+        # get predicted cerebellar data
+        Yhat_parcel, ainfo, _ = ra.agg_data(Yhat, "SUIT3", cerebellum_label, unite_struct = False)
 
-    # save the dataframe
-    filepath = os.path.join(outpath, dataset, f'sc_{dataset}_{ses_id}_{cerebellum}_conn.tsv')
-    summ_df.to_csv(filepath, index = False, sep='\t')
-    return summ_df
+    else: 
+        # there's only one parcel: the whole cerebellum
+        parcels = [1]
+        # aggregate observed values over the whole cerebellum
+        # aggregate predicted values over the whole cerebellum
+        pass
+
+    # add rest condition for control?
+    if add_rest:
+        Yhat_parcel,Y_parcel,info = ra.add_rest_to_data(Yhat_parcel,Y_parcel,info)
+        
+    # Transform into a dataframe with Yhat and Y data 
+    n_subj,n_cond,n_roi = Yhat_parcel.shape
+
+    summary_list = [] 
+    for i in range(n_subj):
+        for r in range(n_roi):
+            info_sub = info.copy()
+            vec = np.ones((len(info_sub),))
+            info_sub["sn"]    = i * vec
+            info_sub["roi"]   = r * vec
+            info_sub["X"]     = Yhat_parcel[i,:,r]
+            info_sub["Y"]     = Y_parcel[i,:,r]
+
+            summary_list.append(info_sub)
+        
+    summary_df = pd.concat(summary_list, axis = 0,ignore_index=True)
+    return summary_df
 
 
 if __name__ == "__main__":
-    
+
+    print("connectivity for MDTB parcellation")
+    D = get_summary_conn(dataset = "WMFS", 
+                        ses_id = 'ses-02', 
+                        cerebellum = "MDTB10", 
+                        cortex = "Icosahedron-1002_Sym.32k",
+                        type = "CondHalf",
+                        add_rest=True,  
+                        conn_dataset = "MDTB", 
+                        conn_method = "L2Regression", 
+                        log_alpha = 8, 
+                        conn_ses_id = "ses-s1")
+
+    # do regression
+    D = ra.run_regress(D,fit_intercept=True)
+
+    D.to_csv(out_dir + '/ROI_MDTB10_conn.tsv',sep='\t')
+
+    #####################################################
+    print("connectivity for hierarchical parcellation")
+    D = get_summary_conn(dataset = "WMFS", 
+                        ses_id = 'ses-02', 
+                        cerebellum = "NettekovenSym34", 
+                        cortex = "Icosahedron-1002_Sym.32k",
+                        type = "CondHalf",
+                        add_rest=True,  
+                        conn_dataset = "MDTB", 
+                        conn_method = "L2Regression", 
+                        log_alpha = 8, 
+                        conn_ses_id = "ses-s1")
+
+    # do regression
+    D = ra.run_regress(D,fit_intercept=True)
+
+    D.to_csv(out_dir + '/ROI_NettekovenSym34_conn.tsv',sep='\t')
+
+    ##############################################################
+    print("connectivity for ROI defined on verbal 2back task of MDTB")
+    D = get_summary_conn(dataset = "WMFS", 
+                        ses_id = 'ses-02', 
+                        cerebellum = "Verbal2Back", 
+                        cortex = "Icosahedron-1002_Sym.32k",
+                        type = "CondHalf",
+                        add_rest=True,  
+                        conn_dataset = "MDTB", 
+                        conn_method = "L2Regression", 
+                        log_alpha = 8, 
+                        conn_ses_id = "ses-s1")
+
+    # do regression
+    D = ra.run_regress(D,fit_intercept=True)
+
+    D.to_csv(out_dir + '/ROI_Verbal2Back_conn.tsv',sep='\t')
+
+    ##########################################################
+    print("ROI-wise comparison for ROI defined on verbal 2back task of MDTB")
     D = ra.get_summary(dataset = "WMFS", 
                 ses_id = 'ses-02', 
                 type = "CondAll", 
@@ -94,4 +161,15 @@ if __name__ == "__main__":
                 cortex_roi = "tpl-fs32k/Verbal2Back.32k",
                 add_rest = True)
     D = ra.run_regress(D,fit_intercept=True)
-    D.to_csv(outdir + '/ROI_Verbal2back.tsv',sep='\t')
+    D.to_csv(out_dir + '/ROI_Verbal2back.tsv',sep='\t')
+
+    #########################################################
+    print("comparison for whole cerebellum vs whole cortex")
+    D = ra.get_summary(dataset = "WMFS", 
+                ses_id = 'ses-02', 
+                type = "CondAll", 
+                cerebellum_roi =None, 
+                cortex_roi = None,
+                add_rest = True)
+    D = ra.run_regress(D,fit_intercept=True)
+    D.to_csv(out_dir + '/ROI_whole.tsv',sep='\t')

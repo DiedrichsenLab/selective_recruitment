0da3ff3cb81da0530aa448c38584692a0cf221d1
Bassel <basselarafat2@gmail.com>
2023-02-27 11:33:58 -0500

    added 'cifs' directories

--- globals.py
+++ globals.py
@@ -3,9 +3,13 @@ from pathlib import Path
 base_dir = '/Volumes/diedrichsen_data$/data/FunctionalFusion'
 if not Path(base_dir).exists():
     base_dir = '/srv/diedrichsen/data/FunctionalFusion'
+if not Path(base_dir).exists():
+    base_dir = '/cifs/diedrichsen/data/FunctionalFusion'
 
 conn_dir = '/Volumes/diedrichsen_data$/data/Cerebellum/connectivity'
 if not Path(conn_dir).exists():
     conn_dir = '/srv/diedrichsen/data/Cerebellum/connectivity'
+if not Path(conn_dir).exists():
+    conn_dir = '/cifs/diedrichsen/data/Cerebellum/connectivity'
 
 atlas_dir = base_dir + '/Atlases'

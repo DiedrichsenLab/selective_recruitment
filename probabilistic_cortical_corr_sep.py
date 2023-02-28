6dd1bf71b39a8a0aff2657f1b37f3aa43b058615
Bassel <basselarafat2@gmail.com>
2023-02-28 10:57:26 -0500

    updates

--- probabilistic_cortical_corr_sep.py
+++ probabilistic_cortical_corr_sep.py
@@ -9,17 +9,23 @@ import nitools as nt
 image= nb.load('/cifs/diedrichsen/data/Cerebellum/ProbabilisticParcellationModel/Atlases/sym_MdPoNiIbWmDeSo_space-MNISymC2_K-32_meth-mixed_cortex-corr.dscalar.nii')
 data= image.get_fdata()
 
-data_LangL=data[13:17,:]
-print(data_LangL.shape)
+data=data[13:16,:]
+print(type(data))
+print(data.shape)
 
 
-thresh= np.percentile(data_LangL,80)
-data_LangL[data_LangL>thresh]=1
-data_LangL[data_LangL<thresh]=0
+thresholded_data = np.zeros_like(data)
+for i in range(data.shape[0]):
+    parcel = data[i, :]
+    threshold = np.nanpercentile(parcel, 80)  # Select top 20% of values, ignoring NaN values
+    thresholded_data[i, parcel >= threshold] = 1
+    thresholded_data[i, parcel < threshold] = 0
+    print(threshold)
+
+print(thresholded_data[0])
 
 
-gifti=nt.make_label_gifti(data_LangL, anatomical_struct='CortexLeft')
-print(type(gifti))
+gifti=nt.make_label_gifti(thresholded_data, anatomical_struct='CortexLeft')
 nb.save(gifti, '/cifs/diedrichsen/data/Cerebellum/Language/Lang.L.label.gii')
 
 

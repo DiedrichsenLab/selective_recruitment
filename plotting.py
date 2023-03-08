208ca2f4411618f226452226290168fd8af7aa1b
lshahsha <lshahsha@uwo.ca>
2023-03-08 09:52:05 -0500

    modified plotting for more flexible scatterplots

--- plotting.py
+++ plotting.py
@@ -81,10 +81,10 @@ def plot_parcellation(parcellation, roi_name):
         ax (axes object)
         roi_num (int) - number corresponding to the region
     """
-    fname = gl.atlas_dir + f'/tpl-SUIT/atl-{parcellation}_space-SUIT_dseg.nii'
-    img = nb.load(fname)
-    # map it from volume to surface
-    img_flat = flatmap.vol_to_surf([img], stats='mode', space = 'SUIT')
+    # fname = gl.atlas_dir + f'/tpl-SUIT/atl-{parcellation}_space-SUIT_dseg.nii'
+    # img = nb.load(fname)
+    # # map it from volume to surface
+    # img_flat = flatmap.vol_to_surf([img], stats='mode', space = 'SUIT')
 
     # get the lookuptable for the parcellation
     lookuptable = nt.read_lut(gl.atlas_dir + f'/tpl-SUIT/atl-{parcellation}.lut')
@@ -98,14 +98,15 @@ def plot_parcellation(parcellation, roi_name):
 
     # get the index for the region
     roi_num = label_info.index(roi_name)
-    roi_flat = img_flat.copy()
-    # convert non-selected labels to nan
-    roi_flat[roi_flat != float(roi_num)] = np.nan
-    # plot the roi
-    ax = flatmap.plot(roi_flat, render="plotly", 
-                      hover='auto', colorbar = False, 
-                      bordersize = 1.5, overlay_type='label', 
-                      label_names=label_info, cmap = cmap)
+    ax = None
+    # roi_flat = img_flat.copy()
+    # # convert non-selected labels to nan
+    # roi_flat[roi_flat != float(roi_num)] = np.nan
+    # # plot the roi
+    # ax = flatmap.plot(roi_flat, render="plotly", 
+    #                   hover='auto', colorbar = False, 
+    #                   bordersize = 1.5, overlay_type='label', 
+    #                   label_names=label_info, cmap = cmap)
 
     return ax, roi_num
 
@@ -199,6 +200,89 @@ def make_scatterplot(dataframe, split='cond_num', labels=None,
 
     # get labels for each data point
     annotate(df, 
+            text_size = 'small', 
+            text_weight = 'regular', 
+            labels = df[split].map(labels))
+    return
+
+
+
+
+def annotate2(dataframe, xlabel, ylabel, labels = 'cond_num', text_size = 'small', text_weight = 'regular'):
+    """
+    annotate data points in the scatterplot
+    Args:
+    dataframe (pd.DataFrame)
+    labels (str,series) - column of the dataframe that is to be used as label
+        or dict that maps the index of the dataframe to a label
+    text_size (str) 
+    text_weight (str)
+    labels (str) 
+    """
+    texts = []
+    if labels is str:
+        labels = dataframe[labels]
+    for i,d in dataframe.iterrows():   
+        text = plt.text(
+                        d[xlabel]+0.001, 
+                        d[ylabel], 
+                        s = labels.loc[i],
+                        horizontalalignment='left', 
+                        size=text_size, 
+                        weight=text_weight
+                        )
+        texts.append(text)
+
+    adjust_text(texts) # make sure you have installed adjust_text
+def make_scatterplot2(dataframe, xlabel, ylabel, xerr, yerr,  split='cond_num', labels=None,
+        colors=None,markers=None):
+    """
+    make scatterplot
+    Args: 
+    dataframe (pd.DataFrame) - 
+            entire dataframe with individual subject data and fitted slopes and intercepts
+    split (str) - column name indicating the different conditions to be plotted
+    labels(dict)    - column name to be used to determine shape of the marker
+    label (str)    - column name to be used to determine the label of the data points
+    height (int)   - int to determine the height of the plot
+    aspect (float) - floating number to determine the aspect ratio of the plot
+    """
+    # do the scatter plot
+    grouped = dataframe.groupby([split])
+    agg_kw = {split:'first',
+              xlabel:np.mean,ylabel: np.mean}
+    df = grouped.agg(agg_kw)
+    
+    df[f"{xlabel}_CI"] = grouped[xlabel].apply(sps.sem) * 1.96
+    df[f"{ylabel}_CI"] = grouped[ylabel].apply(sps.sem)*1.96
+    df['xerr'] = grouped[xerr].apply(sps.sem)*1.96
+    df['yerr'] = grouped[yerr].apply(sps.sem)*1.96
+
+    # add  the appropriate errorbars  
+    plt.errorbar(x = df[xlabel], 
+                 y = df[ylabel], 
+                 yerr = df[yerr],
+                 xerr = df[xerr],
+                 elinewidth=2, 
+                 fmt='none', # no marker will be used when plotting the error bars
+                 color=(0.3,0.3,0.3), 
+                 ecolor=(0.5,0.5,0.5)
+                )
+
+    # # Plot average regression line 
+    # xrange = np.array([df[xlabel].min(),df['X'].max()])
+    # ypred = xrange*df.slope.mean()+df.intercept.mean()
+    # plt.plot(xrange,ypred,'k-')
+
+    # Make scatterplot, determining the markers and colors from the dictionary 
+    ax = sns.scatterplot(data=df, x=xlabel, y=ylabel, style = split, hue = split, s = 100,legend=None,markers=markers,palette=colors)
+
+    # set labels
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    # get labels for each data point
+    annotate2(df, xlabel=xlabel, ylabel=ylabel,
             text_size = 'small', 
             text_weight = 'regular', 
             labels = df[split].map(labels))

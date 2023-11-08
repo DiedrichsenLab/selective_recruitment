796fd541009322120883822a4e3ed720a8530808
lshahsha <44071291+lshahsha@users.noreply.github.com>
2023-11-08 16:09:22 -0500

    updated code for making connectivity weight map

--- plotting.py
+++ plotting.py
@@ -12,7 +12,8 @@ from adjustText import adjust_text # to adjust the text labels in the plots (pip
 
 import nibabel as nb
 import nitools as nt
-# from nilearn import plotting
+import deepdish as dd
+from nilearn import plotting
 import SUITPy.flatmap as flatmap
 import surfAnalysisPy as sa
 
@@ -26,6 +27,7 @@ import selective_recruitment.region as sroi
 import Functional_Fusion.dataset as ds
 import Functional_Fusion.atlas_map as am
 
+# import cortico_cereb_connectivity.scripts.script_plot_weights as wplot
 import cortico_cereb_connectivity.scripts.script_summarize_weights as wplot
 
 # making the scatterplot
@@ -188,31 +190,105 @@ def plot_parcels(parcellation = "NettekovenSym32",
                       overlay_type='label', cmap = cmap)
     return ax
 
-def plot_connectivity_weight(roi_name = "D2R",
-                             method = "L2Regression",
-                             cortex_roi = "Icosahedron1002",
-                             cerebellum_roi = "NettekovenSym68c32",
-                             cerebellum_atlas = "SUIT3",
-                             log_alpha = 8,
-                             view = "lateral", 
-                             dataset_name = "MDTB",
+def get_weight_map(method = "L2Regression", 
+                    cortex_roi = "Icosahedron1002", 
+                    cerebellum_roi = "NettekovenSym68c32",
+                    cerebellum_atlas = "SUIT3", 
+                    extension = '09', 
+                    dataset_name = "Fusion", 
+                    ses_id = "all", 
+                    type = "dscalar"
+                    ):
+
+    """ make cifti image for the connectivity weights
+    Uses the avg model to get the weights, average the weights for voxels within a cerebellar parcel
+    creates cortical maps of average connectivity weights.
+    Args: 
+        method (str) - connectivity method used to estimate weights
+        cortex_roi (str) - cortical tessellation/roi used when training connectivity weights
+        cerebellum_roi (str) - name of the cerebellar roi file you want to get the connectivity weights for
+        cerebellum_atlas (str) - cerebellar atlas used in training connectivity model. "SUIT3" or "MNISym2"
+        log_alpha (float) - log of the regularization parameter used in estimating weights
+        dataset_name (str) - name of the dataset as in functional_fusion framework
+        ses_id (str) - session id used when training the model. "all" for aggregated model over sessions
+        type(str) - type of the cifti you want to create ("pscalar" or "dscalar")
+    Returns:
+        cifti_img (nibabel.Cifti2Image) pscalar cifti image for the cortical maps. ready to be saved!
+    """
+    # make model name
+    m_basename = f"{dataset_name}_{ses_id}_{cortex_roi}_{method}"
+    # load in the connectivity average connectivity model
+    fpath = gl.conn_dir + f"/{cerebellum_atlas}/train/{m_basename}"
+
+    # load the avg model
+    if (len(extension) > 0) and extension[0] != "_":
+        extension = "_" + extension
+    model = dd.io.load(fpath + f"/{m_basename}{extension}_avg.h5")
+
+    # get the weights
+    weights = model.coef_
+
+    # prepping the parcel axis file
+    ## make atlas object first
+    atlas_fs, _ = am.get_atlas("fs32k", gl.atlas_dir)
+
+    # load the label file for the cortex
+    label_fs = [gl.atlas_dir + f"/tpl-fs32k/{cortex_roi}.{hemi}.label.gii" for hemi in ["L", "R"]]
+
+    # get parcels for the neocortex
+    _, label_fs = atlas_fs.get_parcel(label_fs, unite_struct = False)
+
+    # getting parcel info for the cerebellum 
+    atlas_suit, _ = am.get_atlas(cerebellum_atlas, gl.atlas_dir)
+
+    # load the label file for the cerebellum
+    label_suit = gl.atlas_dir + f"/tpl-SUIT/atl-{cerebellum_roi}_space-SUIT_dseg.nii"
+
+    # getting parcels for the cerebellum
+    atlas_suit.get_parcel(label_suit)
+
+    # get the average cortical weights for each cerebellar parcel
+    weights_parcel, labels = ds.agg_parcels(weights.T, atlas_suit.label_vector, fcn=np.nanmean)
+
+    # preping the parcel axis
+    ## load the lookup table for the cerebellar parcellation to get the names of the parcels
+    index,colors,labels = nt.read_lut(gl.atlas_dir + f"/tpl-SUIT/atl-{cerebellum_roi}.lut")
+
+    # make cifti
+    if type == "pscalar":
+        # create parcel axis for the cortex (will be used as column axis in pscalar file)
+        p_axis = atlas_fs.get_parcel_axis()
+
+        # make a parcel cifti and pass the p_axis 
+        row_axis = nb.cifti2.ScalarAxis(labels[1:]) # rows are maps for each cerebellar parcel
+        # make header
+        ## rows are maps corresponding to cerebellar parcels
+        ## columns are cortical tessels
+        header = nb.Cifti2Header.from_axes((row_axis, p_axis)) 
+        cifti_img = nb.Cifti2Image(weights_parcel.T, header=header)
+    elif type == "dscalar":
+        # get the maps for left and right hemispheres
+        surf_map = []
+        for label in atlas_fs.label_list:
+            # loop over regions within the hemisphere
+            label_arr = np.zeros([weights_parcel.T.shape[0], label.shape[0]])
+            for p in np.arange(1, weights_parcel.T.shape[0]):
+                for i in np.unique(label):            
+                    np.put_along_axis(label_arr[p-1, :], np.where(label==i)[0], weights_parcel.T[p-1,i-1], axis=0)
+            surf_map.append(label_arr)
+
+        cifti_img = atlas_fs.data_to_cifti(surf_map, row_axis=labels[1:])
+        
+    return cifti_img
+
+def plot_connectivity_weight(cifti_img, 
+                             view = "lateral",
+                             roi_name = "D2R",
                              cmap = "coolwarm",
                              colorbar = True, 
-                             cscale = [-0.0001, 0.0001],
-                             ses_id = "ses-s1"):
+                             cscale = [-0.0001, 0.0001]):
     """
     """
-    # get connectivity weight maps for the selected region
-    cifti_img = wplot.get_weight_map(method = method,
-                                    cortex_roi = cortex_roi,
-                                    cerebellum_roi = cerebellum_roi,
-                                    cerebellum_atlas = cerebellum_atlas,
-                                    log_alpha = log_alpha,
-                                    dataset_name = dataset_name,
-                                    ses_id = ses_id,
-                                    type = "dscalar"
-                                    )
-
     # get the cortical weight map corresponding to the current
     ## get parcel axis from the cifti image
     parcel_axis = cifti_img.header.get_axis(0)
@@ -223,7 +299,7 @@ def plot_connectivity_weight(roi_name = "D2R",
     # get the map for the selected region for left and right hemispheres
     weight_roi_list = [weight_map_list[h][idx, :] for h in [0, 1]]
 
-    surfs = [gl.atlas_dir + f"/tpl-fs32k/tpl_fs32k_hemi-{hemi}_inflated.surf.gii" for hemi in ['L', 'R']]
+    surfs = [gl.atlas_dir + f"/tpl-fs32k/tpl-fs32k_hemi-{hemi}_inflated.surf.gii" for hemi in ['L', 'R']]
     ax = []
     for h, hemi in enumerate(['left', 'right']):
 
@@ -271,7 +347,6 @@ def plot_significant_activity_cortex(data,
     
     return cifti_img
     
-
 
 def plot_rgb_map(data_rgb, 
                  atlas_space = "SUIT3", 
@@ -469,16 +544,24 @@ def plot_mds3_plotly(x, y,z,
 
 
 if __name__ == "__main__":
-    plot_connectivity_weight(roi_name = "D2R",
-                             method = "L2Regression",
-                             cortex_roi = "Icosahedron1002",
-                             cerebellum_roi = "NettekovenSym32",
-                             cerebellum_atlas = "SUIT3",
-                             log_alpha = 8,
-                             view = "lateral", 
-                             dataset_name = "MDTB",
-                             cmap = "coolwarm",
-                             colorbar = True, 
-                             cscale = [-0.0001, 0.0001],
-                             ses_id = "ses-s1")
+    plot_conn_weight(method = "L2Regression",
+                     dataset_name = "MDTB",
+                     cortex_roi = "Icosahedron1002",
+                     extension = 'A8',
+                     ses_id = "all",
+                     cerebellum_roi = "NettekovenSym32",
+                     cerebellum_atlas = "SUIT3",
+                     parcel_name = "M3R")
+    # plot_connectivity_weight(roi_name = "D2R",
+    #                          method = "L2Regression",
+    #                          cortex_roi = "Icosahedron1002",
+    #                          cerebellum_roi = "NettekovenSym32",
+    #                          cerebellum_atlas = "SUIT3",
+    #                          log_alpha = 8,
+    #                          view = "lateral", 
+    #                          dataset_name = "MDTB",
+    #                          cmap = "coolwarm",
+    #                          colorbar = True, 
+    #                          cscale = [-0.0001, 0.0001],
+    #                          ses_id = "ses-s1")
     pass
